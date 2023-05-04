a3928934ccbb8665954923ce167f3cdec7d1124e
kvit7 <kvit72v@yandex.ru>
2023-05-04 18:57:37 +0300

    В классе ExitCommand реализован метод execute. В классе CashMachine изменен метод main.

--- src/ATM/CashMachine.java
+++ src/ATM/CashMachine.java
@@ -1,16 +1,21 @@
 package ATM;
 
 import ATM.command.CommandExecutor;
+import ATM.exception.InterruptOperationException;
 
 import java.util.Locale;
 
 public class CashMachine {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
-        Operation operation;
-        do {
-            operation = ConsoleHelper.askOperation();
-            CommandExecutor.execute(operation);
-        } while (operation != Operation.EXIT);
+        try {
+            Operation operation;
+            do {
+                operation = ConsoleHelper.askOperation();
+                CommandExecutor.execute(operation);
+            } while (operation != Operation.EXIT);
+        } catch (InterruptOperationException ignored) {
+            ConsoleHelper.writeMessage("Terminated. Thank you for visiting ATM. Good luck!");
+        }
     }
 }
--- src/ATM/command/ExitCommand.java
+++ src/ATM/command/ExitCommand.java
@@ -1,8 +1,17 @@
 package ATM.command;
 
+import ATM.ConsoleHelper;
+import ATM.exception.InterruptOperationException;
+
 public class ExitCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Do you really want to exit? <y,n>");
+        String result = ConsoleHelper.readString();
+        if (result != null && "y".equals(result.toLowerCase())) {
+            ConsoleHelper.writeMessage("Thank you for visiting ATM. Good luck!");
+        } else {
 
+        }
     }
 }
