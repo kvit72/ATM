fa51627ba73eac14a96b169c5e6d5db83a7d028e
kvit7 <kvit72v@yandex.ru>
2023-05-04 16:08:45 +0300

    В классе CashMachine реализован метод main. В класс CurrencyManipulator добавлен метод addAmount. В класс ConsoleHelper добавлены методы askCurrencyCode, getValidTwoDigits.

--- src/ATM/CashMachine.java
+++ src/ATM/CashMachine.java
@@ -1,7 +1,18 @@
 package ATM;
 
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+
+        String[] digits = ConsoleHelper.getValidTwoDigits(currencyCode);
+        int denomination = Integer.parseInt(digits[0]);
+        int count = Integer.parseInt(digits[1]);
+
+        CurrencyManipulator manipulatorByCurrencyCode = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+        manipulatorByCurrencyCode.addAmount(denomination, count);
 
     }
 }
