8da8a6a2931c29b570254a44e9ba8d4f1a3bd99f
kvit7 <kvit72v@yandex.ru>
2023-05-05 11:26:31 +0300

    В класс CurrencyManipulator добавлен метод withdrawAmount и isAmountAvailable. В классе WithdrawCommand реализован метод execute.

--- src/ATM/CurrencyManipulator.java
+++ src/ATM/CurrencyManipulator.java
@@ -1,7 +1,8 @@
 package ATM;
 
-import java.util.Map;
-import java.util.TreeMap;
+import ATM.exception.NotEnoughMoneyException;
+
+import java.util.*;
 
 public class CurrencyManipulator {
     private String currencyCode;
@@ -34,5 +35,52 @@ public class CurrencyManipulator {
 
     public boolean hasMoney() {
         return !denominations.isEmpty();
+    }
+
+    public boolean isAmountAvailable(int expectedAmount) {
+        return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException {
+        int sum = expectedAmount;
+        HashMap<Integer, Integer> copyDenominations = new HashMap<>(denominations);
+
+        ArrayList<Integer> keys = new ArrayList<>(this.denominations.keySet());
+
+        Collections.sort(keys);
+        Collections.reverse(keys);
+
+        TreeMap<Integer, Integer> resultMap = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+
+        for (Integer denomination : keys) {
+            final int key = denomination;
+            int value = copyDenominations.get(key);
+            while (true) {
+                if (sum < key || value == 0) {
+                    copyDenominations.put(key, value);
+                    break;
+                }
+                sum -= key;
+                value--;
+
+                if (resultMap.containsKey(key))
+                    resultMap.put(key, resultMap.get(key) +1);
+                else
+                    resultMap.put(key, 1);
+            }
+        }
+
+        if (sum > 0)
+            throw new NotEnoughMoneyException();
+        else {
+            this.denominations.clear();
+            this.denominations.putAll(copyDenominations);
+        }
+        return resultMap;
     }
 }
--- src/ATM/command/WithdrawCommand.java
+++ src/ATM/command/WithdrawCommand.java
@@ -1,8 +1,49 @@
 package ATM.command;
 
+import ATM.ConsoleHelper;
+import ATM.CurrencyManipulator;
+import ATM.CurrencyManipulatorFactory;
+import ATM.exception.InterruptOperationException;
+import ATM.exception.NotEnoughMoneyException;
+
+import java.util.Map;
+
 public class WithdrawCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Withdrawing...");
 
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+
+        while (true) {
+            try {
+                ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
+                String s = ConsoleHelper.readString();
+                if (s == null) {
+                    ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                } else {
+                    try {
+                        int amount = Integer.parseInt(s);
+                        boolean isAmountAvailable = manipulator.isAmountAvailable(amount);
+                        if (isAmountAvailable) {
+                            Map<Integer, Integer> denominations = manipulator.withdrawAmount(amount);
+                            for (Integer item : denominations.keySet()) {
+                                ConsoleHelper.writeMessage("\t" + item + " - " + denominations.get(item));
+                            }
+
+                            ConsoleHelper.writeMessage(String.format("%d %s was withdrawn successfully", amount, currencyCode));
+                            break;
+                        } else {
+                            ConsoleHelper.writeMessage("Not enough money on your account, please try again");
+                        }
+                    } catch (NumberFormatException e) {
+                        ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                    }
+                }
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("Exact amount is not available");
+            }
+        }
     }
 }
